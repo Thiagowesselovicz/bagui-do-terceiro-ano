c17795f30596388484f48701824bd38229e88968
Thiagowesselovicz <127854839+Thiagowesselovicz@users.noreply.github.com>
2024-04-26 11:51:04 -0300

    data vest

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-11-24T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-07-01T00:00:00");
+const tempoObjetivo2 = new Date("2024-11-03T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-08T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-14T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
